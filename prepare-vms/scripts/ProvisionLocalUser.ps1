d6e7a739981876008db48d00b5efc80387a23e37
Paul Broadwith <paul@pauby.com>
2019-09-20 14:15:25 +0100

    Add user to local Users group [skip ci]

--- prepare-vms/scripts/ProvisionLocalUser.ps1
+++ prepare-vms/scripts/ProvisionLocalUser.ps1
@@ -1,2 +1,3 @@
 $pwd = ConvertTo-SecureString -String 'Password01' -AsPlainText -Force
-New-LocalUser "selfservice" -Password $pwd -FullName "Self Service User" -Description "Allows you to use Self Service."
+New-LocalUser -Name "selfservice" -Password $pwd -FullName "Self Service User" -Description "Allows you to use Self Service." -UserMayNotChangePassword -PasswordNeverExpires
+Add-LocalGroupMember -Group 'Users' -Member 'selfservice'

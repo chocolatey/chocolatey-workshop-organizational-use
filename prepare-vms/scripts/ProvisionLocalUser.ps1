e551bfbb2f8b1ef5a6c33ac395124e351ec594fd
Paul Broadwith <paul@pauby.com>
2019-09-20 15:16:57 +0100

    Suppress PSScriptAnalyzer rule [skip ci]

--- prepare-vms/scripts/ProvisionLocalUser.ps1
+++ prepare-vms/scripts/ProvisionLocalUser.ps1
@@ -1,3 +1,6 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingConvertToSecureStringWithPlainText", "", Justification = "We need to embed the password")]
+Param ()
+
 $pwd = ConvertTo-SecureString -String 'Password01' -AsPlainText -Force
 New-LocalUser -Name "selfservice" -Password $pwd -FullName "Self Service User" -Description "Allows you to use Self Service." -UserMayNotChangePassword -PasswordNeverExpires
 Add-LocalGroupMember -Group 'Users' -Member 'selfservice'

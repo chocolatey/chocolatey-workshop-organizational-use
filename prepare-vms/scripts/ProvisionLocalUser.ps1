aae835965d42a8725de4494cb330fde9fb44ec6b
Paul Broadwith <paul@pauby.com>
2019-09-22 12:10:15 +0100

    (maint) Allow selfservice user to RDP [skip ci]

--- prepare-vms/scripts/ProvisionLocalUser.ps1
+++ prepare-vms/scripts/ProvisionLocalUser.ps1
@@ -3,4 +3,4 @@ Param ()
 
 $pwd = ConvertTo-SecureString -String 'Password01' -AsPlainText -Force
 New-LocalUser -Name "selfservice" -Password $pwd -FullName "Self Service User" -Description "Allows you to use Self Service." -UserMayNotChangePassword -PasswordNeverExpires
-Add-LocalGroupMember -Group 'Users' -Member 'selfservice'
+'Users', 'Remote Desktop Users' | ForEach-Object { Add-LocalGroupMember -Group $_ -Member 'selfservice' }

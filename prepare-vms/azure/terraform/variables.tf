9624a4b8dd6a65bd1bee92e1314fa7eca13664c7
Gary Ewan Park <gep13@gep13.co.uk>
2019-09-22 22:19:14 +0100

    Updating workshop image name [skip ci]

--- prepare-vms/azure/terraform/variables.tf
+++ prepare-vms/azure/terraform/variables.tf
@@ -30,7 +30,7 @@ variable "admin_username" {
 }
 
 variable "workshop_image" {
-  default = "windows_2016_96"}
+  default = "windows_2016_214"}
 
 variable "vm_size" {
   default = "Standard_D4s_v3"

3e0783f52a5395628a036d6324383da1cbc76062
Paul Broadwith <paul@pauby.com>
2019-11-04 23:06:45 -0500

    Update workshop image name [skip ci]

--- prepare-vms/azure/terraform/variables.tf
+++ prepare-vms/azure/terraform/variables.tf
@@ -30,7 +30,7 @@ variable "admin_username" {
 }
 
 variable "workshop_image" {
-  default = "windows_2016_366"}
+  default = "windows_2016_390"}
 
 variable "vm_size" {
   default = "Standard_D4s_v3"
